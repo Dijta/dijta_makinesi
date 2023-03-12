d6f1a61b5c0ed0f1373e2a564e9c6920f98c6d42
Ö. Alkan Tonyalı <127542810+Dijta@users.noreply.github.com>
2023-03-12 18:35:36 +0000

    Delete test outputs

--- core/dmb.py
+++ core/dmb.py
@@ -4,7 +4,6 @@ import text.perception as pre
 def run():
   yz = read.dma_reader("","dmb")
 
-  print(yz)
   a,b,c,d = pre.dmb_perception(yz)
 
   print(a,b,c,d)
--- text/perception.py
+++ text/perception.py
@@ -3,20 +3,15 @@ def dmb_perception(mtn=[]):
   tur_a = False
   olay_a = False
   dahil_a = False
-  print(mtn.count(":ad:"))
  
   if mtn.count(":ad:") == 1:     
-    print("a")
     ad_a = True
   if  mtn.count(":tür:")  ==  1:
    tur_a = True
-   print("a")
   if  mtn.count(":olay:") ==  1: 
    olay_a = True
-   print("a")
   if  mtn.count(":dahil:") ==  1:
-    dahil_a = True
-    print("a")    
+    dahil_a = True  
   return ad_a,tur_a,olay_a,dahil_a
   
       
